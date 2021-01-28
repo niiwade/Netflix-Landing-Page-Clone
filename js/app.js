b6b9ad6af06d386affe8d1dae32f8540a7f4e652
niiwade <niilantewade77@gmail.com>
2021-01-28 20:55:04 +0000

    second push

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 function toogleVide0(){
     const trailer = document.querySelector('trailer');
+    const video = document.querySelector('video')
+    trailer.classList.toogle('active')
+    video.currentTime = 0;
+    video.pause();
 }
