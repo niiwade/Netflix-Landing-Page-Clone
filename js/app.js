91ff28bb2e701300a50af4d7338e4aebc26bbde8
niiwade <niilantewade77@gmail.com>
2021-01-29 15:31:15 +0000

    tenth push

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-function toogleVide0(){
-    const trailer = document.querySelector('trailer');
-    const video = document.querySelector('video')
-    trailer.classList.toogle('active')
+function toggleVideo(){
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active')
     video.currentTime = 0;
     video.pause();
 }
+
